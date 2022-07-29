fb0c7990b4a48f17dd557fd135d4e66f8fc444cb
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2022-07-29 10:38:39 +0800

    add getter

--- src/main/java/projectmetallurgy/metallurgy/fire/CommandRegistry.java
+++ src/main/java/projectmetallurgy/metallurgy/fire/CommandRegistry.java
@@ -2,6 +2,7 @@ package projectmetallurgy.metallurgy.fire;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraftforge.client.event.RegisterClientCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +15,9 @@ public class CommandRegistry {
         event.getDispatcher().register(Commands.literal("fire").then(Commands.literal("set").then(Commands.argument("value", IntegerArgumentType.integer()).executes(ctx -> {
             FireValue.set(ctx.getArgument("value", Integer.class));
             return 1;
-        }))));
+        }))).then(Commands.literal("get").executes(ctx -> {
+            ctx.getSource().sendSuccess(new TextComponent(FireValue.get()+""), false);
+            return 1;
+        })));
     }
 }
